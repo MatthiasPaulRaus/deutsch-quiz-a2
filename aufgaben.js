124ed1f8121171c825060ec5c550eeb33c4119d9
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-03-22 17:25:25 +0100

    Add files via upload

---
+++ aufgaben.js
@@ -0,0 +1,222 @@
+const aufgaben = [
+	{
+	"question": "Was macht ihr mit euren Kindern am Wochenende?",
+	"option1": "Mal sehen, ob sie meine Tante anrufen.",
+	"option2": "Wir fahren zusammen in die Berge.",
+	"option3": "Ich möchte meinen Onkel besuchen.",
+	"answer": "2"
+	},
+	{
+	"question": "Wohin sollen wir das Bild hängen?",
+	"option1": "Hm, am besten neben das Regal.",
+	"option2": "Ich würde es in den Schrank stellen.",
+	"option3": "Keine Ahnung, vielleicht in der Ecke.",
+	"answer": "1"
+	},
+	
+	{
+	"question": "Hast du meine Brille gesehen?",
+	"option1": "Schau mal, der Schrank.",
+	"option2": "Tut mir leid, aber ich habe keine.",
+	"option3": "Ja, ich glaube sie liegt auf dem Tisch.",
+	"answer": "3"
+	},
+	{
+	"question": "Entschuldigung, wo ist das Obst?",
+	"option1": "Ich habe es in den Kühlschrank gelegt.",
+	"option2": "Da ist noch Gemüse in der Tasche.",
+	"option3": "Er steht in der Küche.",
+	"answer": "1"
+	},
+	{
+	"question": "Was möchtet ihr denn im Urlaub machen?",
+	"option1": "Wir wollen auf einem Bauernhof leben.",
+	"option2": "Vielleicht eine Reise nach Italien.",
+	"option3": "Eine Radtour durch die Stadt.",
+	"answer": "2"
+	},
+	{
+	"question": "Ich würde gern einen Koch-Kurs machen. Und du?",
+	"option1": "Er würde lieber einen Computer-Kurs machen.",
+	"option2": "Sie interessiert sich für Tanzen.",
+	"option3": "Kochen mag ich nicht. Tanzen ist mir lieber.",
+	"answer": "3"
+	},
+	{
+	"question": "Wissen Sie, wo man einen Foto-Kurs machen kann?",
+	"option1": "Ja, dazu musst du an den Stand fahren.",
+	"option2": "Das kannst du nur in den Bergen machen.",
+	"option3": "Bestimmt gibt es Kurse im Kulturzentrum.",
+	"answer": "3"
+	},
+	{
+	"question": "Welches T-Shirt gefällt dir am besten?",
+	"option1": "Das mit dem Metallica-Logo gefällt uns sehr.",
+	"option2": "Das mit dem Beatles-Foto finde ich am besten.",
+	"option3": "Das mit dem Alpen-Foto gefällt ihr ganz gut.",
+	"answer": "2"
+	},
+	{
+	"question": "Wie lange dauert das Tennis-Turnier?",
+	"option1": "Ab Anfang Mai.",
+	"option2": "Am nächsten Monat.",
+	"option3": "Vom 14. bis zum 16. Juni.",
+	"answer": "3"
+	},
+	{
+	"question": "Seit wann ist das Fitness-Center geöffnet?",
+	"option1": "Schon seit über einem Monat.",
+	"option2": "Ich glaube, nächsten Monat.",
+	"option3": "Ungefähr bis Ende des Monats.",
+	"answer": "1"
+	},
+	
+	
+	
+	{
+	"question": "Haben Sie Lust, das Konzert zu besuchen?",
+	"option1": "Nein, sie mag keine Konzerte.",
+	"option2": "Ja, aber am Wochenende haben wir keine Zeit.",
+	"option3": "Ja, sie haben Lust auf ein Konzert.",
+	"answer": "2"
+	},
+	{
+	"question": "Sagen Sie mal, ist das Schwimmbad immer so voll?",
+	"option1": "Schade, dass Sie nicht mitkommen.",
+	"option2": "Ich hoffe, dass Sie schwimmen können.",
+	"option3": "Ja, gehen wir lieber in den Park.",
+	"answer": "3"
+	},
+	{
+	"question": "Was machst du in deiner Freizeit?",
+	"option1": "Wenn ich Zeit habe, treffe ich meine Freunde.",
+	"option2": "Sie treffen ihre Freunde, wenn sie Zeit haben.",
+	"option3": "Wenn ich meine Freunde treffe, habe ich Freizeit.",
+	"answer": "1"
+	}, 
+	{
+	"question": "Entschuldigung, können Sie mir helfen?",
+	"option1": "Ja, ich helfe Ihnen gern.",
+	"option2": "Ja, sie hilft Ihnen gern.",
+	"option3": "Ja, ihr helft Ihnen gern.",
+	"answer": "1"
+	},
+	{
+	"question": "Hast du meinen Schlüssel gesehen?",
+	"option1": "Nein, ich habe er nicht gesehen.",
+	"option2": "Ja, sie liegt hier.",
+	"option3": "Ja, ich gebe ihn dir gleich.",
+	"answer": "3"
+	},
+	{
+	"question":"Wie war der Film?",
+	"option1": "Sehr spannend. Den kann ich dir wirklich empfehlen.",
+	"option2": "Schlecht. Den können Sie nicht empfehlen.",
+	"option3": "Er hat ihnen nicht gefallen.",
+	"answer": "1"
+	},
+	{
+	"question": "Es ist schon halb acht!",
+	"option1": "Halb acht ist mir zu früh.",
+	"option2": "Wissen Sie, ob das Geschäft noch offen ist?.",
+	"option3": "Ich weiß nicht, ob heute geöffnet ist.",
+	"answer": "2"
+	}, 
+	{
+	"question": "Wie wird das Wetter am Wochenende sein?",
+	"option1": "Gestern war das Wetter schlecht.",
+	"option2": "Ich weiß nicht, wie es sein wird.",
+	"option3": "Ich weiß nicht, ob es regnet.",
+	"answer": "2"
+	}, 
+	{
+	"question": "Wo seid ihr am liebsten im Urlaub?",
+	"option1": "In Italien, am Strand.",
+	"option2": "Mein Traum ist ein Wochenende in Paris.",
+	"option3": "Leider hatten noch keinen Urlaub.",
+	"answer": "1"
+	}, 
+	{
+	"question": "Waren Sie schon einmal auf dieser Insel?",
+	"option1": "Nein, sie war noch nie da.",
+	"option2": "Ja, er war im Sommer auf ihr.",
+	"option3": "Ja, wir waren schon zweimal da.",
+	"answer": "3"
+	},
+	
+	
+	
+	
+	
+	{
+	"question": "Auf wen freut ihr euch am meisten?",
+	"option1": "Auf Weihnachten.",
+	"option2": "Wir freuen ganz besonders auf unseren Onkel.",
+	"option3": "Ich freue mich auf meine Tante. Sie kommt morgen.",
+	"answer": "2"
+	},
+	{
+	"question": "Wann sind die Weihnachsferien?",
+	"option1": "Ende Dezember bis Anfang Januar.",
+	"option2": "Entweder im Dezember oder im Januar.",
+	"option3": "Von Dezember bis Januar",
+	"answer":"1"
+	},
+	{
+	"question": "Woher kommt ihr so spät?",
+	"option1": "Vom Einkaufen.",
+	"option2": "Zum Einkaufen.",
+	"option3": "Wir haben morgen mehr Zeit.",
+	"answer": "1"
+	}, 
+	{
+	"question": "Wohin gehst du nach der Arbeit?",
+	"option1": "Meine Freunde gehen zum Sportplatz.",
+	"option2": "Sie muss noch zum Arzt gehen.",
+	"option3": "Hm, ich gehe noch zum Einkaufen.",
+	"answer": "3"
+	},
+	{
+	"question": "Hast du Lust auf einen Spaziergang?",
+	"option1": "Nein, das ist meine Idee.",
+	"option2": "Ja, das ist eine gute Idee.",
+	"option3": "Ja, das ist nicht sehr praktisch.",
+	"answer": "2"
+	},
+	{
+	"question": "Durftest du als Kind am Abend lange fernsehen?",
+	"option1": "Nein, ich musste schon früh ins Bett gehen.",
+	"option2": "Ich wollte lange fernsehen.",
+	"option3": "Ich wollte schon früh ins Bett gehen.",
+	"answer": "1"
+	},
+	{
+	"question": "Von wem hast du das Buch bekommen?",
+	"option1": "Hm, das weiß sie nicht mehr.",
+	"option2":"Ich glaube, von meiner Kollegin.",
+	"option3":"Das ist wirklich sehr langweilig.",
+	"answer":"2"
+	},
+	{   
+	 "question": "Was machen Sie mit dem alten Fahrrad?",
+	 "option1":"Ich will ein neues Fahrrad.",
+	 "option2":"Sie will eine Rad-Tour machen.",
+	 "option3":"Ich werde es meiner Schwester schenken.",
+	"answer":"3"
+	},
+	
+	{
+	"question":"Wissen Sie, wer zum Seminar mitkommt?",
+	 "option1":"Ich glaube, dass fast alle mitkommen.",
+	 "option2":"Viele Kollegen wollen helfen.",
+	 "option3":"Viele konnten nicht mitkommen.",
+	"answer":"1"
+	},
+	{
+	"question":"Wissen Sie, ob man Sportschuhe tragen muss?",
+	"option1":"Viele müssen besondere Schuhe tragen.",
+	"option2":"Keine Ahnung. Fragen Sie den Reiseleiter!",
+	"option3":"Die Reiseleiterin trägt keine Sportschuhe.",
+	 "answer":"2"
+	}
+	];
